988d8d31f89e1a3fdf01bce74ce5cb4378a543bc
Victor Suciu <victor@victors-mbp-4.lan>
2020-05-24 17:38:15 -0700

    Added map labeler

--- src/labeler.py
+++ src/labeler.py
@@ -1,2 +1,15 @@
 
-# Labeling functions go here
+# Returns a list of map labels
+# Labels: 0 = no map in solution, >0 = map exists in solution
+# Each index in all_labels labels the corresponding index in all_problems
+def maps(all_problems):
+    all_labels = []
+    label = 0
+    for problem in all_problems:
+        for solution in problem.solutions:
+            for line in solution:
+                if 'map<' in line:
+                    label += 1
+        all_labels.append(label)
+    return all_labels
+    
