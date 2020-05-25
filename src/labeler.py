c6fbeb43a436d466c83a3a1b834f4e650e9d9028
hollowsunsets <isabelgiang@gmail.com>
2020-05-24 22:04:28 -0700

    Fix parser, add code that reads problems.json

--- src/labeler.py
+++ src/labeler.py
@@ -1,2 +1,26 @@
 
 # Labeling functions go here
+import json
+
+PROBLEMS = [] 
+
+
+# Creates a list of dictionaries containing problems of the form:
+# { 
+#   number : <number>,
+#   statement : "baojgiodfjiodf",
+#   solution  : [ iujgiodjgdiofgj, difjgdfiogjiofgjdiofg, dkfopdfkgopdfkgo, ..., ifjgiofdjgo ]
+# }
+
+def read_problems():
+    with open('problems.json') as f:
+        for line in f:
+            problem = json.loads(line)
+            print(json.dumps(problem, indent=4, sort_keys=True)) # Printed for demonstrative purposes. Feel free to remove.
+            PROBLEMS.append(problem)
+
+def main():
+    read_problems()
+
+if __name__ == '__main__':
+    main()
