1155528a80271f73098151c83656dca78a9f7361
jesrun <jessicarunandy@gmail.com>
2020-05-25 22:12:14 -0700

    Added linked list labeler and modified format of functions to match data-reading

--- src/labeler.py
+++ src/labeler.py
@@ -1,84 +1,97 @@
 
+# Labeling functions go here
+import json
+
+
 # Returns a list of map labels
 # Labels: 0 = no map in solution, >0 = map exists in solution
 # Each index in all_labels labels the corresponding index in all_problems
 def maps(all_problems):
     all_labels = []
-    label = 0
+
     for problem in all_problems:
-        for solution in problem.solutions:
-            for line in solution:
-                if 'map<' in line:
-                    label += 1
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'map<' in line.lower():
+                    label = 1
         all_labels.append(label)
     return all_labels
 
-# Returns a list of vector labels
-# Labels: 0 = no vector in solution, >0 = vector exists in solution
-# Each index in all_labels labels the corresponding index in all_problems
-def vectors(all_problems):
+
+def lists(all_problems):
     all_labels = []
-    label = 0
+
     for problem in all_problems:
-        for solution in problem.solutions:
-            for line in solution:
-                if 'vector<' in line:
-                    label += 1
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'list<' in line.lower() or 'vector<' in line.lower():
+                    label = 1
         all_labels.append(label)
     return all_labels
 
-# Returns a list of set labels
-# Labels: 0 = no set in solution, >0 = set exists in solution
-# Each index in all_labels labels the corresponding index in all_problems
+
 def sets(all_problems):
     all_labels = []
-    label = 0
+
     for problem in all_problems:
-        for solution in problem.solutions:
-            for line in solution:
-                if 'set<' in line:
-                    label += 1
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'set<' in line.lower():
+                    label = 1
         all_labels.append(label)
     return all_labels
 
-# Returns a list of stack labels
-# Labels: 0 = no stack in solution, >0 = stack exists in solution
-# Each index in all_labels labels the corresponding index in all_problems
+
 def stacks(all_problems):
     all_labels = []
-    label = 0
+
     for problem in all_problems:
-        for solution in problem.solutions:
-            for line in solution:
-                if 'stack<' in line:
-                    label += 1
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'stack<' in line.lower():
+                    label = 1
         all_labels.append(label)
     return all_labels
 
-# Returns a list of queue labels
-# Labels: 0 = no queue in solution, >0 = queue exists in solution
-# Each index in all_labels labels the corresponding index in all_problems
+
 def queues(all_problems):
     all_labels = []
-    label = 0
+
     for problem in all_problems:
-        for solution in problem.solutions:
-            for line in solution:
-                if 'queue<' in line:
-                    label += 1
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'queue<' in line.lower():
+                    label = 1
         all_labels.append(label)
     return all_labels
 
-# Returns a list of priority queue labels
-# Labels: 0 = no priority queue in solution, >0 = priority queue exists in solution
-# Each index in all_labels labels the corresponding index in all_problems
+
 def priority_queues(all_problems):
     all_labels = []
-    label = 0
+
     for problem in all_problems:
-        for solution in problem.solutions:
-            for line in solution:
-                if 'priority_queue<' in line:
-                    label += 1
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'priority_queue<' in line.lower():
+                    label = 1
+        all_labels.append(label)
+    return all_labels
+
+
+def linked_lists(all_problems):
+    all_labels = []
+
+    for problem in all_problems:
+        label = 0
+        for solution in problem['solutions']:
+            for line in solution.split('\n'):
+                if 'Node' in line.lower() or 'LinkedList<' in line.lower():
+                    label = 1
         all_labels.append(label)
     return all_labels
