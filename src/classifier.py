0fa93862b532edc2d8069b1334ff00cda0434f68
Victor Suciu <victor@victors-mbp-4.lan>
2020-05-26 00:18:17 -0700

    forgot to change the variable in the print statement

--- src/classifier.py
+++ src/classifier.py
@@ -44,7 +44,7 @@ def gridsearch(labels, random_trials, c_vals, g_vals):
     max_g = 0
     
     for trial in range(random_trials):
-        print('Trial:', i + 1)
+        print('Trial:', trial + 1)
         data = ModelData(all_embeddings, labels)
         total = len(data.validation_labels)
         for c in c_vals:
