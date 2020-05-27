1b21b97f63428c0507bcaca87c7e88fbbb0b9359
Victor Suciu <victor@victors-mbp-4.lan>
2020-05-27 15:57:25 -0700

    added embeddings file to save time added class weights to gridsearch

--- src/classifier.py
+++ src/classifier.py
@@ -1,4 +1,4 @@
-from embedder import get_embeddings
+from embedder import get_embeddings, get_embeddings_from_file
 from modeldata import ModelData
 import labeler as Label
 import random
@@ -23,53 +23,104 @@ def read_problems():
 
 
 all_problems = read_problems()
-print('\n\n\n', all_problems[0]['solutions'], '\n\n\n')
+# print('\n\n\n', all_problems[0]['solutions'], '\n\n\n')
 
 map_labels = Label.maps(all_problems)
 list_labels = Label.lists(all_problems)
+set_labels = Label.sets(all_problems)
+stack_labels = Label.stacks(all_problems)
+queue_labels = Label.queues(all_problems)
+priority_queue_labels = Label.priority_queues(all_problems)
+
 loop_labels = []
 nested_loop_labels = []
 
-print(list_labels)
-print(len(list_labels))
-print('Positive:', list_labels.count(1), '/ 763')
+# print(list_labels)
 
-all_embeddings = get_embeddings(all_problems)
+
+# exit()
+
+all_embeddings = get_embeddings_from_file()
+print('Single Embedding:', len(all_embeddings[0]))
 print('\nEmbeddings Length:', len(all_embeddings), '\n')
 
+data = ModelData(all_embeddings, list_labels)
+data.set_data_kfold(10, 0)
+print(data.training_labels)
 
-def gridsearch(labels, random_trials, c_vals, g_vals):
-    max_correct = 0
+def kfold_cross_validation(labels, k, c, gamma, weight): 
+    correct = np.zeros(k)
+    weights = {0:weight, 1:1.0}
+
+    for index in range(k):
+        data = ModelData(all_embeddings, labels)
+        data.set_data_kfold(k, index)
+        
+        my_svm = svm.SVC(kernel='rbf', C=c, gamma=gamma, class_weight=weights)
+        my_svm.fit(data.training_embeddings, data.training_labels)
+        # print(my_svm.support_vectors_)
+        
+        for i in range(len(data.validation_labels)):
+            # print(my_svm.predict([data.validation_embeddings[i]])[0], end='', flush=True)
+            
+            if my_svm.predict([data.validation_embeddings[i]])[0] == data.validation_labels[i]:
+                correct[index] += 1
+        correct[index] /= len(data.validation_labels)
+
+    # print(correct)
+    return np.mean(correct)
+
+
+def gridsearch(labels, random_trials, c_vals, g_vals, weights):
+    positive = labels.count(1)
+    print(int((positive / 763) * 100) / 100, '% positive, or', positive, '/ 763')
+    max_accuracy = 0
     max_c = 0
     max_g = 0
+    max_w = 0
+    k = 10
     
-    for trial in range(random_trials):
-        print('Trial:', trial + 1)
-        data = ModelData(all_embeddings, labels)
-        total = len(data.validation_labels)
-        for c in c_vals:
-            for gamma in g_vals:
-                correct = 0
-                my_svm = svm.SVC(kernel='rbf', C=c, gamma=gamma)
-                my_svm.fit(data.training_embeddings, data.training_labels)
-    
-                for i in range(total):
-                    if my_svm.predict([data.validation_embeddings[i]])[0] == data.validation_labels[i]:
-                        correct += 1
-                if correct > max_correct:
-                    max_correct = correct
+    count = 1
+    for c in c_vals:
+        for gamma in g_vals:
+            for weight in weights:
+                if count % len(weights) == 0:
+                    print(count, end=' ', flush=True)
+
+                accuracy = kfold_cross_validation(labels, k, c, gamma, weight)
+
+                if accuracy > max_accuracy:
+                    max_accuracy = accuracy
                     max_c = c
                     max_g = gamma
+                    max_w = weight
+
+                count += 1
     
-    print('\nAccuracy:', max_correct / total)
-    print('C =', max_c, '\ngamma =', max_g)
+    print('\nAccuracy:', max_accuracy)
+    print('C =', max_c, '\ngamma =', max_g, 'weight =', max_w)
+
+    
 
 
-c_range = np.logspace(-3, 3, 50)
-g_range = np.logspace(-3, 3, 50)
-
+c_range = np.logspace(-1, 2, 10)
+g_range = np.logspace(-2, 0, 10)
+w_range = np.linspace(0, 0.6, 60)
+print(w_range)
 print('\n\nOptimizing SVM hyperparameters for lists...')
-gridsearch(list_labels, 10, c_range, g_range)
+gridsearch(list_labels, 10, c_range, g_range, w_range)
+
+print('\n\nOptimizing SVM hyperparameters for sets...\n')
+gridsearch(set_labels, 10, c_range, g_range, w_range)
+
+print('\n\nOptimizing SVM hyperparameters for stacks...')
+gridsearch(stack_labels, 10, c_range, g_range, w_range)
+
+print('\n\nOptimizing SVM hyperparameters for queues...')
+gridsearch(queue_labels, 10, c_range, g_range, w_range)
+
+print('\n\nOptimizing SVM hyperparameters for priority queues...')
+gridsearch(priority_queue_labels, 10, c_range, g_range, w_range)
 
 print('\n\nOptimizing SVM hyperparameters for maps...')
-gridsearch(map_labels, 10, c_range, g_range)
+gridsearch(map_labels, 10, c_range, g_range, w_range)
--- src/modeldata.py
+++ src/modeldata.py
@@ -2,18 +2,16 @@ import random
 
 class ModelData:
     def __init__(self, embeddings, labels):
-        self.all_embeddings = embeddings
-        self.all_labels = labels
+        self.all_embeddings = list(embeddings)
+        self.all_labels = list(labels)
 
         self.training_embeddings = []
         self.training_labels = []
 
         self.validation_embeddings = []
         self.validation_labels = []
-
-        self.set_data()
     
-    def set_data(self):
+    def set_data_7030(self):
         for i in range(len(self.all_labels)):
             if random.random() <= 0.7 :
                 self.training_embeddings.append(self.all_embeddings[i])
@@ -21,5 +19,13 @@ class ModelData:
             else:
                 self.validation_embeddings.append(self.all_embeddings[i])
                 self.validation_labels.append(self.all_labels[i])
-
-
+    
+    def set_data_kfold(self, k, index):
+        remove_index = int(len(self.all_labels) / k * index)
+        
+        for i in range(int(len(self.all_labels) / k)):
+            self.validation_embeddings.append(self.all_embeddings.pop(remove_index))
+            self.validation_labels.append(self.all_labels.pop(remove_index))
+        
+        self.training_embeddings = self.all_embeddings
+        self.training_labels = self.all_labels
